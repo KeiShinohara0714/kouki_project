da5441dc14edfed63ad90f36a1093bed8160a53c
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-06 11:43:17 +0000

    0206

--- ex1-1.js
+++ ex1-1.js
@@ -4,11 +4,4 @@ function txtchange1() {
 function imgchange1() {
   document.getElementById('logo').src = './image/hat trick.jpeg';    
 }
-function show_innerHTMLtxt1(){
-  var result1 = document.getElementById('txt1').innerHTML;
-alert(result1);    
-}
-function show_innerHTML1(){
-  var result = document.getElementById('logo').innerHTML;
-alert(result);    
-}
+
--- ex1-2.js
+++ ex1-2.js
@@ -4,11 +4,3 @@ function txtchange2() {
 function imgchange2() {
   document.getElementById('logo').src = './image/cycle hits.jpeg';    
 }
-function show_innerHTMLtxt2(){
-  var result2 = document.getElementById('txt2').innerHTML;
-alert(result1);    
-}
-function show_innerHTML2(){
-  var result = document.getElementById('logo').innerHTML;
-alert(result);    
-}
