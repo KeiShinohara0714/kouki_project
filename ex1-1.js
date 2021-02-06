5d49dab39d9501a824cec90f64064d67aa53d893
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-06 08:09:04 +0000

    0206

---
+++ ex1-1.js
@@ -0,0 +1,15 @@
+function txtchange1() {
+  document.getElementById('txt1').innerHTML = 'ハットトリック!';  
+}
+function imgchange1() {
+  document.getElementById('logo').src = './image/hat trick.jpeg';    
+}
+function show_innerHTMLtxt1(){
+  var result1 = document.getElementById('txt1').innerHTML;
+alert(result1);    
+}
+
+function show_innerHTML(){
+  var result = document.getElementById('logo').innerHTML;
+alert(result);    
+}
