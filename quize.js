714535b12cb509ac1fd17232a50ff9cc61e25370
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-07 01:55:00 +0000

    0207

---
+++ quize.js
@@ -0,0 +1,13 @@
+function nazo(){
+  if(document.getElementById('answer').value =='坂本勇人選手')
+  {alert('正解です!');}
+  else if(document.getElementById('answer').value =='坂本勇人選手')
+    {alert('正解です!')} 
+    else{alert('違います!');}
+ }
+function hinto1(){
+   alert('現在、読売ジャイアンツに所属している選手です!')
+ }
+function hinto2(){
+   alert('2020年シーズンでは右打者として最年少で2000安打を達成された選手です!')
+ }    
