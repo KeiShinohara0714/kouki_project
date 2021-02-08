392702496ace58de56eb2aa56e12cf2852666b87
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-08 22:35:31 +0000

    0209

--- ex1-1.js
+++ ex1-1.js
@@ -1,5 +1,5 @@
 function txtchange1() {
-  document.getElementById('txt1').innerHTML = 'ハットトリック!';  
+  document.getElementById('txt1').innerHTML = 'ハットトリック';  
 }
 function imgchange1() {
   document.getElementById('logo1').src = './image/hat trick.jpeg';    
